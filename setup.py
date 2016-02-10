3b913acae36df4fbddbe2fe684c512ae6e6d1805
houghb <houghb@uw.edu>
2016-02-10 13:18:43 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,9 @@
 import os
 from distutils.core import setup
 
-pts = dict(name=Blake,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=myproject,
-            package_data=PACKAGE_DATA,
-            requires=REQUIRES)
+opts = dict(name="Blake",
+            description="my project for UWSEDS",
+            packages=['myproject', 'myproject/tests'])
 
 
 if __name__ == '__main__':
